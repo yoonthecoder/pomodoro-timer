e01703372cb551f0e3e4c0b260358f43c96f6728
Yoon Kim <imyoonkim@gmail.com>
2021-03-31 23:55:05 -0700

    add song

--- main.js
+++ main.js
@@ -1,77 +1,72 @@
+const song = $('.song')[0];
+
+
+// Play the music
+$('#play-btn').click(()=>{
+  checkPlaying(song);
+})
+
+// Create a function to stop and play the music
+const checkPlaying = (song) => {
+  if(song.paused) {
+    song.play();
+    $('#play-btn').removeClass('fa-play').addClass('fa-stop');
+  } else {
+    song.pause();
+    $('#play-btn').removeClass('fa-stop').addClass('fa-play');
+  }
+};
+
+let duration = 1500;
+const outline =  $('.progress-ring-circle')[0];
+const outlineLength = outline.getTotalLength();
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
+
+// Get the timer working 
+song.ontimeupdate = () => {
+  let currentTime = song.currentTime;
+  let elapsed = duration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  
+  // Animate the circle
+  let progress = outlineLength - (currentTime / duration) * outlineLength;
+  outline.style.strokeDashoffset = progress;
+  $('.time-display')[0].textContent = `${minutes}:${seconds}`;
+ 
+  if(currentTime >= duration){
+    song.pause();
+    song.currentTime = 0;
+    $('#play-btn').removeClass('fa-stop').addClass('fa-play');
+}
+}
+
+const reset = () => {
+    song.pause();
+    song.currentTime = 0;
+    $('#play-btn').removeClass('fa-stop').addClass('fa-play');
+}
+
 // Tab options 
 $('.tab span').click(function(e){
   $('.tab-item').removeClass('tab-active');
   $(e.currentTarget).addClass('tab-active');
+
+  if(e.currentTarget.textContent === "short break"){
+    reset();
+    $('.time-display')[0].textContent = "5:00";
+    duration = 300;
+  } else if(e.currentTarget.textContent === "pomodoro"){
+    reset();
+    $('.time-display')[0].textContent = "25:00";
+    duration = 1500;
+  } else if(e.currentTarget.textContent === "long break"){
+    reset();
+    $('.time-display')[0].textContent = "10:00";
+    duration = 600;
+  }
 })
-
-// Timer 
-var minutes = 25;
-var seconds = "00";
-var isPaused = true;
-
-$('.timer-minutes').html(minutes);
-$('.timer-seconds').html(seconds);
-
-// Start the timer
-function play() {
-  if(isPaused) {
-    timer();
-    $('#play-btn').removeClass('fa-play').addClass('fa-stop');
-  } else{
-    $('#play-btn').removeClass('fa-stop').addClass('fa-play');
-  }
-}
-
-function timer() {
-  isPaused = false;
-  
-  minutes = 24;
-  seconds = 59;
-
-  $('.timer-minutes').html(minutes);
-  $('.timer-seconds').html(seconds);
-
-  var minutes_interval = setInterval(minutesTimer,60000);
-  var seconds_interval = setInterval(secondsTimer,1000);
-
-  function minutesTimer() {
-    minutes = minutes - 1;
-    $('.timer-minutes').html(minutes);
-  }
-
-  function secondsTimer() {
-    seconds = seconds - 1;
-    $('.timer-seconds').html(seconds);
-    
-    if(seconds < 1) {
-      if (minutes < 1) {
-        clearInterval(minutes_interval);
-        clearInterval(seconds_interval);
-        isPaused = true;
-      }
-      seconds = 60;
-    }
-  }
-}
-
-$('.play-btn').click(()=> {play()});
-
-// Reset the timer
-$('.reset-btn').click(function() {
-  $('.timer-minutes').html(25);
-  $('.timer-seconds').html("00");
-})
-
-// Settings Modal 
-
-$('.close-btn , .settings-btn').click(
-  function(){
-    $('.settings-modal').hide()   
-}) 
-
-$('.setting-btn').click(function() {
-    $('.settings-modal').show()})
-
 // Settings - Theme Colours
 
 // Blue theme
